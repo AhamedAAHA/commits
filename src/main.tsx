ea9150f6cf0b687ea454865e824b8036bdfe94de
AhamedAAHA <hubaibahamedaaha@gmail.com>
2026-07-16 01:43:04 +0530

    Update src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,22 @@ import "./index.css";
 import { AppErrorBoundary } from "./components/AppErrorBoundary";
 import { AdminProvider } from "./context/AdminContext";
 import { ContentProvider } from "./context/ContentContext";
+import { CursorProvider } from "./context/CursorContext";
+import { ThemeProvider } from "./context/ThemeContext";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppErrorBoundary>
-      <ContentProvider>
-        <AdminProvider>
-          <App />
-        </AdminProvider>
-      </ContentProvider>
+      <ThemeProvider>
+        <CursorProvider>
+          <ContentProvider>
+            <AdminProvider>
+              <App />
+            </AdminProvider>
+          </ContentProvider>
+        </CursorProvider>
+      </ThemeProvider>
     </AppErrorBoundary>
   </StrictMode>,
 );
